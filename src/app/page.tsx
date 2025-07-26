84950b94d0b8b6a388afb1f4c4a8441635f73e8b
Mauricio Ignacio Pastora <mauriciopastora@gmail.com>
2025-07-26 01:47:53 -0400

    deploying website to AWS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,7 +55,7 @@ export default function Page() {
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
                 <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-                  Mauricio Pastora
+                  Mauricio Ignacio Pastora
                 </h1>
                 <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
                   Developer portfolio showcasing my projects, skills,
